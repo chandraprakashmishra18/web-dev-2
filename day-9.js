418fb5f2a7d425e3afc2785ca3c14951d979230b
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-02-03 15:14:22 +0530

    changes made

--- day-9.js
+++ day-9.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded"),function () {
-    const profile = 
+    const profile = document.getElementById("profile");
+    profile.style.backgroundColor="Red";
+    profile.style.padding="15px"
+    profile.style.textAlign="center"
 }
