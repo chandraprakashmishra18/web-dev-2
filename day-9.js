e64523520bca551085fb49331c8c178fab7b8fa6
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-02-03 15:27:17 +0530

    okk commit changes

--- day-9.js
+++ day-9.js
@@ -1,6 +1,12 @@
-document.addEventListener("DOMContentLoaded"),function () {
     const profile = document.getElementById("profile");
     profile.style.backgroundColor="Red";
     profile.style.padding="15px"
     profile.style.textAlign="center"
+
+const task2 = document.getElementsByClassName("taskclass");
+console.log(task2);
+for(let i=0;i<task2.length;i++){
+    task2[i].style.color="voilet"
+    task[i].style.fontSize="40px"
 }
+console.log(task2.length)
