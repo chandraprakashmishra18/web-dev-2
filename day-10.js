6bc440090f55d0a65a646a096bc6a1f86ce0ca80
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-02-04 13:58:56 +0530

    ok

--- day-10.js
+++ day-10.js
@@ -2,5 +2,5 @@ const newParagraph = document.createElement("p");
 newParagraph.textContent = "This is a dynamically created paragaraph .";
 console.log(newParagraph);
 
-const container = document.getElementById("conatiner");
+const container = document.getElementById("container");
 container.appendChild(newParagraph);
