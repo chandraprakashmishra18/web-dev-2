1b9dc7153790f35588b2004b9b188a8e0f0d0618
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-02-16 15:52:33 +0530

    asynchronus commitment for callback hell

--- day-14.js
+++ day-14.js
@@ -23,11 +23,27 @@
 console.log("Start...");
 function login(uname, password, cb) {
     setTimeout (()=>{
-        cb("Hello World");
-    },5000);
+        cb({uname: uname, isLoggedIn:true , Message: "Login Successfully"});
+    },1000);
 }
-myFunction((data)=>{
-    console.log(data);
+function getVideolist(email,cb) {
+    setTimeout (()=>{
+    cb([{title:"video 1"},{title: "Video 2"}]);
+},2000);
+}
+function getVideoDetail(video,cb){
+    setTimeout(()=>{
+        cb({title: video.title,description: "This is the req video" +video.title});
+    },2000);
+}
+login( "prashant" , "password123" , (userdata)=>{
+    console.log(userdata);
+    getVideolist(userdata.uname, (VideoList)=>{
+        console.log(VideoList);
+        getVideoDetail(VideoList[0] , (videoDetail)=>{
+            console.log(videoDetail);
+        });
+    });
 });
 console.log("End..");
 
