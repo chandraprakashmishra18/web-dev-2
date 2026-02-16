f9f31bad94f10166cebfae9eef03cd88aa1ba694
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-02-16 15:14:42 +0530

    asynchronus fncn basics

--- day-14.js
+++ day-14.js
@@ -1,6 +1,22 @@
-console.log("Start...")
-function myFunction(){
-    console.log("Hello world");
+// console.log("Start...")
+// function myFunction(){
+//     console.log("Hello world");
+// }
+// myFunction();
+// console.log("End...");
+// function myFunction() {
+//     setTimeout( ()=>{
+//         return " Hello World";
+//     },5000);
+// }
+// console.log(myFunction);
+console.log("Start...");
+function myFunction(cb) {
+    setTimeout (()=>{
+        cb("Hello World");
+    },5000);
 }
-myFunction();
-console.log("End...");
+myFunction((data)=>{
+    console.log(data);
+});
+console.log("End..");
