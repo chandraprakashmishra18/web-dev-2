4194e919e4aecfb71ca5f223f5a66c7a6cf757d9
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2026-03-19 00:09:17 +0530

    okk

--- day-19.js
+++ day-19.js
@@ -12,20 +12,20 @@
 //     }
 // }
 // fetchData("london")
-// async function fetchData(city) {
-//     try {
-//         const API_key = "1de0181b6dd424271dec30b076344656";
-//         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=1de0181b6dd424271dec30b076344656`);
+async function fetchData(city) {
+    try {
+        const API_key = "1de0181b6dd424271dec30b076344656";
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=1de0181b6dd424271dec30b076344656`);
 
-//         const data = await response.json();
-//         console.log(city);
-//         console.log(data.main.temp);
-//         console.log(data.main.humidity);
-//     } catch (err) {
-//         console.error(err);
-//     }
-// }
-// fetchData("london")
+        const data = await response.json();
+        console.log(city);
+        console.log(data.main.temp);
+        console.log(data.main.humidity);
+    } catch (err) {
+        console.error(err);
+    }
+}
+fetchData("london")
 
 
 
